539c3017df35521dc6588cd4ad07f137c42b3579
smothe-git <110886770+smothe-git@users.noreply.github.com>
2022-08-17 12:44:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-      shared_credentials_files = "/home/ec2-user/.aws/credentials"
-      shared_config_files      = "/home/ec2-user/.aws/config"
-      profile                 = "default"
-    }
+  region = "us-west-2"
+}
