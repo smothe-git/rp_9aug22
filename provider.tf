56d1e584a86b316608348a17cba26bea83f8b045
smothe-git <110886770+smothe-git@users.noreply.github.com>
2022-08-17 10:32:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = "us-west-2"
-}
+      shared_credentials_files = "~/.aws/credentials"
+      shared_config_files      = "~/.aws/config"
+      profile                 = "default"
+    }
